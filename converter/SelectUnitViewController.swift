5dac8567c116b696018e5232e28e111ac8fbf02c
Chakpiwat Pacharajindawat <gameraroma@gmail.com>
2018-11-24 14:47:33 +0700

    delete comment

--- converter/SelectUnitViewController.swift
+++ converter/SelectUnitViewController.swift
@@ -61,15 +61,4 @@ class SelectUnitViewController: UIViewController, UITableViewDataSource, UITable
         selectedUnit = unitList[indexPath.row]
         super.performSegue(withIdentifier: "unitSelected", sender: self)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
